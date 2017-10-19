151a0e4ae056b22747876368ab3fbda527735201
Kalvin Ball <kball66816@gmail.com>
2017-10-19 09:37:57 -0700

    Moved Addon List to AddonChargeView

--- PatientManagement.ViewModel/Services/DialogService.cs
+++ PatientManagement.ViewModel/Services/DialogService.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PatientManagement.Model;
 using System.Windows;
-using PatientManagement.Model;
 
 namespace PatientManagement.ViewModel.Services
 {
@@ -12,18 +7,17 @@ namespace PatientManagement.ViewModel.Services
     {
         public DialogService(Patient patient)
         {
-            dialogResult = false;
             newDialogResult = MessageBox.Show("Do you want to add additional encounters to this patient?", "Return new patient",
                     MessageBoxButton.YesNo);
         }
 
         public DialogService(AddonCharge addon)
         {
-            dialogResult = false;
             newDialogResult = MessageBox.Show("Do you want to reuse this Addon?", "Return new Addon",
                 MessageBoxButton.YesNo);
 
         }
+
         private bool dialogResult;
 
         private readonly MessageBoxResult newDialogResult;
