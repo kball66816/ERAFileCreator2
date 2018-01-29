54914a54989159a87a9c185b6f74de8422adf1ec
Kalvin Ball <kball66816@gmail.com>
2018-01-28 17:16:58 -0800

    Added UploadFile ViewModel

---
+++ PatientManagement.ViewModel/UploadFileViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using PatientManagement.ViewModel.Services;
+using Common.Common;
+
+namespace PatientManagement.ViewModel
+{
+    public class UploadFileViewModel
+    {
+        public UploadFileViewModel()
+        {
+            UploadFileCommand = new Command(GetUploadFile,CanUploadFile);
+        }
+
+        private ICommand UploadFileCommand { get; set; }
+
+        private static void GetUploadFile(object obj)
+        {
+            UploadFile.TextFileUpload();
+        }
+
+        private bool CanUploadFile(object obj)
+        {
+            bool canUpload = obj != null;
+
+            return canUpload;
+        }
+    }
+}
