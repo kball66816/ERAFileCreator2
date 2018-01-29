310c56205d6523d5d5e086f2f5cc21f772dac22b
Kalvin Ball <kball66816@gmail.com>
2018-01-28 17:01:33 -0800

    Added upload controller

---
+++ ERAFileCreator2/Common.Common/UploadFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Common.Common
+{
+    public static class UploadFile
+    {
+        public static string Contents { get; private set; }
+
+        public static void SelectFileForUpload()
+        {
+            var file = new OpenFileDialog
+            {
+                InitialDirectory = "C:\\",
+                Filter = "txt files (*.txt)|*.txt|837 files(*.837)|*.837|All files(*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (var streamReader = new StreamReader(file.OpenFile()))
+                    {
+                        Contents = streamReader.ReadLine();
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+
+
+
+        }
+    }
+}
