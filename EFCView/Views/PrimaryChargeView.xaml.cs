a2aa8cc10370d30b60ec66f3574c00e97406b046
Kalvin Ball <kball66816@gmail.com>
2018-04-28 14:42:00 -0700

    re-introduced ui rules

--- EFCView/Views/PrimaryChargeView.xaml.cs
+++ EFCView/Views/PrimaryChargeView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Common.Common.Extensions;
@@ -17,85 +18,12 @@ namespace EraFileCreator.Views
             InitializeComponent();
         }
 
-        private void PrimaryChargesAmountInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            if (e.ToString() == ".")
-            {
-
-            }
-        }
-
-        private void BillIdInput_GotFocus(object sender, System.Windows.RoutedEventArgs e)
+        private void HightlightAllText(object sender, RoutedEventArgs e)
         {
             InputRules.HighlightAllText(sender);
         }
 
-        private void PatientCopayInput_GotFocus(object sender, System.Windows.RoutedEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryProcedureCodeInput_GotFocus(object sender, System.Windows.RoutedEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryModifierOneInput_GotFocus(object sender, System.Windows.RoutedEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryModifierTwoInput_GotFocus(object sender, System.Windows.RoutedEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryModifierThreeInput_GotFocus(object sender, System.Windows.RoutedEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryModifierFourInput_GotFocus(object sender, System.Windows.RoutedEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryPaidAmountInput_GotFocus(object sender, System.Windows.RoutedEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryChargesAmountInput_GotFocus(object sender, System.Windows.RoutedEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PatientCopayInput_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryChargesAmountInput_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryPaidAmountInput_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            InputRules.HighlightAllText(sender);
-        }
-
-        private void PrimaryChargesAmountInput_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            InputRules.IgnoreDecimalInput(sender, e);
-        }
-
-        private void PrimaryPaidAmountInput_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            InputRules.IgnoreDecimalInput(sender,e);
-        }
-
-        private void PatientCopayInput_PreviewKeyDown(object sender, KeyEventArgs e)
+        private void IgnoreDecimalInput(object sender, KeyEventArgs e)
         {
             InputRules.IgnoreDecimalInput(sender, e);
         }
